3af6ba61330a5eca666f87b8ceeb5a3e165de94d
Angelitoo777 <angeloropeza1604@gmail.com>
2025-09-17 14:14:41 -0400

    prueba

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { initializeSequelize } from '../config/mysql.database.js'
+import { routesOfClients } from '../routes/client.routes.js'
+
+const app = express()
+
+app.use(express.json())
+app.use('/api', routesOfClients)
+
+app.get('/', (req, res) => {
+  res.send('Hola vercel')
+})
+
+export default async (req, res) => {
+  try {
+    const sequelize = await initializeSequelize()
+    await sequelize.sync()
+    app(req, res)
+  } catch (error) {
+    console.error('Error connecting your database.', error)
+    res.status(500).send('Database connection error')
+  }
+}
