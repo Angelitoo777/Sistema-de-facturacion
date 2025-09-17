139f0a1938b8debcba3a10b71b8c53fd15d1ca07
Angelitoo777 <angeloropeza1604@gmail.com>
2025-09-17 12:59:47 -0400

    Se crearon todas las tablas pero aun faltan las relaciones entre las mismas

---
+++ models/products.model.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../config/mysql.database.js'
+
+export const Products = sequelize.define('products', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  stock: {
+    type: DataTypes.NUMBER,
+    allowNull: false
+  }
+})
