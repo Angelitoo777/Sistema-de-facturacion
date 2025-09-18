89ca88e6dd4bc0a92023eeb712194114c9b0ed3f
Angelitoo777 <angeloropeza1604@gmail.com>
2025-09-18 11:03:07 -0400

    Se le agrego al cors la direccion del frontend

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -1,7 +1,7 @@
 import cors from 'cors'
 
 const corsOptions = {
-  origin: ['https://sistema-de-facturacion-eight.vercel.app', 'http://localhost:3000'],
+  origin: ['https://sistema-de-facturacion-eight.vercel.app', 'http://localhost:3000', 'https://my-billing-app-tan.vercel.app/'],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
